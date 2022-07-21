b8b78ce12e27f5fa9570bc3576e5cea4fb3aef2e
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-20 23:39:13 -0300

    Primeiro teste no componente App

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,38 +1,29 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
 import App from '../App';
 
 describe('Teste o componente App', () => {
+  beforeEach(() => {
+    render(<MemoryRouter><App /></MemoryRouter>);
+  });
   test('Se o topo da aplicação contém um conjunto fixo de links de navegação', () => {
-    render(<App />);
-
-    const homeLink = screen.getByRole('Link', {
-      name: /Home/i,
-    });
+    const homeLink = screen.getByText(/home/i);
     expect(homeLink).toBeInTheDocument();
 
-    const aboutLink = screen.getByRole('Link', {
-      name: /About/i,
-    });
-
+    const aboutLink = screen.getByText(/about/i);
     expect(aboutLink).toBeInTheDocument();
 
-    const favoriteLink = screen.getByRole('Link', {
-      name: /Favorite Pokémons/i,
-    });
-
+    const favoriteLink = screen.getByText(/Favorite Pokémons/i);
     expect(favoriteLink).toBeInTheDocument();
   });
 });
 
-test('Teste se a aplicação é redirecionada para a página inicial', () => {
-  const { history } = render(<App />);
-  const homeLink = screen.getByRole('Link', {
-    name: /Home/i,
-  });
-  expect(homeLink).toBeInTheDocument();
-  userEvent.click(homeLink);
-
-  const { pathname } = history.location;
-  expect(pathname).toBe('/');
-});
+// test('Teste se a aplicação é redirecionada para a página inicial', async () => {
+//   renderWithRouter(<App />);
+//   const homeLink = screen.getByRole('Link', {
+//     name: /Home/i,
+//   });
+//   expect(homeLink).toBeInTheDocument();
+//   userEvent.click(homeLink);
+// });
