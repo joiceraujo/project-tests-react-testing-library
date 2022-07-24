a6a11dbb1675c7b3228d00b34e069454beb15843
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-24 16:39:34 -0300

    Requisito 5

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { MemoryRouter } from 'react-router-dom';
+import App from '../App';
+
+
+describe('Teste o componente Pokedex', () => {
+    beforeEach(() => {
+      render(<MemoryRouter><App /></MemoryRouter>);
+    });
+
+    test('Teste se a página contém um heading h2 com o texto Encountered pokémons', () => {
+        const textPokemons = screen.getByRole('heading', { 
+            name: /Encountered pokémons/i,
+            level: 2,
+        });
+        expect(textPokemons).toBeInTheDocument();
+      });
+
+      test('Teste se é exibido o próximo pokémon da lista quando o botão Próximo pokémon é clicado', () => {
+        const next = 'Proximo pokémon';
+        const nextPokemon = screen.getByTestId(/next-pokemon/i);
+        expect(nextPokemon).toBeInTheDocument();
+        userEvent.click(nextPokemon);
+      });
+
+    test('Teste se a Pokédex tem os botões de filtro', () => {
+        const buttons = screen.getByRole('button', {
+            name: /electric/i 
+        });
+        const pokemonId = screen.getAllByTestId(/pokemon-type-button/i);
+        expect(buttons).toBeDefined();
+        userEvent.click(buttons);
+        expect(pokemonId).toBeDefined();
+    });
+
+  test('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    const buttonReset = screen.getByRole('button', {
+        name: /All/i 
+    });
+    expect(buttonReset).toBeInTheDocument();
+    userEvent.click(buttonReset);
+    
+});
+});
+
