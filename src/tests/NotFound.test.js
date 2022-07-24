90bb9c70b3a0afe9806a385904eddc3b41d800ee
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-24 11:05:52 -0300

    Lint req 4

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -4,22 +4,22 @@ import { MemoryRouter } from 'react-router-dom';
 import NotFound from '../pages/NotFound';
 
 describe('Teste o componente NotFound', () => {
-    beforeEach(() => {
-      render(<MemoryRouter><NotFound /></MemoryRouter>);
-    });
+  beforeEach(() => {
+    render(<MemoryRouter><NotFound /></MemoryRouter>);
+  });
 
-    test('Teste se a página contém um heading h2 com o texto Page requested not found', () => {
-      const textNotFound = screen.getByRole('heading', {
-        name: /Page requested not found/i,
-        level: 2,
-      });
-      expect(textNotFound).toBeInTheDocument();
+  test('Teste se a página contém um heading h2 com texto Page requested not found', () => {
+    const textNotFound = screen.getByRole('heading', {
+      name: /Page requested not found/i,
+      level: 2,
     });
+    expect(textNotFound).toBeInTheDocument();
+  });
 
-    test('Teste se a página mostra a imagem', () => {
-      const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
-      const imgAlt = 'Pikachu crying because the page requested was not found';
-      const img = screen.getByAltText(imgAlt);
-      expect(img).toHaveAttribute('src', imgSrc);
-    });
+  test('Teste se a página mostra a imagem', () => {
+    const imgSrc = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const imgAlt = 'Pikachu crying because the page requested was not found';
+    const img = screen.getByAltText(imgAlt);
+    expect(img).toHaveAttribute('src', imgSrc);
+  });
 });
