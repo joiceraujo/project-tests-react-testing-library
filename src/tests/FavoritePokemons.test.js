231ecadfcdf9326dafc88492e13575212f75767d
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-23 22:40:34 -0300

    Tentativa 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import FavoritePokemons from '../pages/FavoritePokemons';
 import { MemoryRouter } from 'react-router-dom';
 import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
 
 describe('Teste o componente Favorite Pokemons', () => {
   beforeEach(() => {
     render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
     });
   
-test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
-    const noFavorite = screen.getByText(/No favorite pokemon found/i);
-    expect(noFavorite).toBeInTheDocument();
+    test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
+        const noFavorite = screen.getByText(/No favorite pokemon found/i);
+        expect(noFavorite).toBeInTheDocument();
     });
 });
