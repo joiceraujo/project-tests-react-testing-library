23b8fe997c779d7fdbf2c29b34ec9242542dc924
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-24 09:42:33 -0300

    Tentativa stryker do req 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import FavoritePokemons from '../pages/FavoritePokemons';
 
 describe('Teste o componente Favorite Pokemons', () => {
