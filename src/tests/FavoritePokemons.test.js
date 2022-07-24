03a28c478eb088ec1e714983570e1a8ff91b2027
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-23 21:50:32 -0300

    Resolvendo Lint

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { FavoritePokemons } from '../pages';
+import  FavoritePokemons  from '../pages/FavoritePokemons';
 import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 describe('Teste o componente Favorite Pokemons', () => {
     beforeEach(() => {
-      render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
+    render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
     });
   
-
 test('Teste se é exibida na tela a mensagem No favorite pokemon found, caso a pessoa não tenha pokémons favoritos', () => {
     const favoritePokemons = screen.getByText(/No favorite pokemon found/i);
     expect(favoritePokemons).toBeInTheDocument();
