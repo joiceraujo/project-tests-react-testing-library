c3838190ae7bf97d63a80d6ffe53bcd4a482ff3f
Joice Araújo <joicearaujoedu@gmail.com>
2022-07-23 22:54:48 -0300

    tentativa lint 5

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -6,9 +6,9 @@ import FavoritePokemons from '../pages/FavoritePokemons';
 describe('Teste o componente Favorite Pokemons', () => {
   beforeEach(() => {
     render(<MemoryRouter><FavoritePokemons /></MemoryRouter>);
-});
+  });
   test('Teste se é exibida na tela a mensagem No favorite pokemon found', () => {
-        const noFavorite = screen.getByText(/No favorite pokemon found/i);
-        expect(noFavorite).toBeInTheDocument();
-    });
+    const noFavorite = screen.getByText(/No favorite pokemon found/i);
+    expect(noFavorite).toBeInTheDocument();
+  });
 });
